73152eb9274f13339f44864f7206185f5170d440
Amin Foroutan <aminforoutan7@gmail.com>
2020-10-20 15:34:28 +0330

    added the mongoose logic and fixed linting problems

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-import express from 'express';
-import morgan from 'morgan';
-import path from 'path';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import routes from './Routes';
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
+import routes from './Routes'
+import seedDB from './Models/seed'
 
-const app = express();
-app.set('views', path.resolve(__dirname, './views'));
-app.set('view engine', 'ejs');
+const app = express()
+app.set('views', path.resolve(__dirname, './views'))
+app.set('view engine', 'ejs')
 
-app.use(morgan('dev'));
-app.use(express.static(path.relative(__dirname, './public')));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('very random key'));
+mongoose.connect('mongodb://localhost/amin_foroutan', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  // eslint-disable-next-line no-console
+  console.log('connected to the mongoose server')
+  seedDB()
+})
 
-app.use('/', routes());
+app.use(morgan('dev'))
+app.use(express.static(path.relative(__dirname, './public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser('very random key'))
+
+app.use('/', routes())
 
 app.get('/complex/:who', (req, res) => {
-  const result = req.params.who || 'no name specified';
-  res.send(result);
-});
+  const result = req.params.who || 'no name specified'
+  res.send(result)
+})
 
 app.use((req, res) => {
   res.status(404).render('404', {
-    title: '404 not found!',
-  });
-});
+    title: '404 not found!'
+  })
+})
 
-app.listen('3000');
+app.listen('3000')
