b21fdaa096553fad91e0c588549abcf06f9bee7c
Amin Foroutan <aminforoutan7@gmail.com>
2020-10-20 15:30:11 +0330

    adding the seeding logic of users[check desc] -creating user schema and models -creating seed the js funcion

---
+++ Models/seed.js
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose'
+import { hash, compare } from 'bcrypt'
+
+const SALT_ROUNDS = 12
+const userSchema = new Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
+})
+
+// eslint-disable-next-line prefer-arrow-callback
+userSchema.pre('save', async function (next) {
+  const user = this
+  if (!user.isModified('password')) return next()
+
+  try {
+    const hashedPass = await hash(this.password, SALT_ROUNDS)
+    user.password = hashedPass
+  } catch (err) {
+    return next(err)
+  }
+
+  return next()
+})
+
+async function comparePass (candidate) {
+  const result = await compare(candidate, this.password)
+  return result
+}
+
+userSchema.methods.comparePass = comparePass
+
+const UserModel = model('User', userSchema)
+
+const seedUserDB = async () => {
+  const resluts = await UserModel.findOne()
+  if (!resluts) {
+    const user = new UserModel({
+      username: 'resistance2200',
+      password: 'Batman2200Amin'
+    })
+    user.save()
+  }
+  return resluts
+}
+
+export default seedUserDB
