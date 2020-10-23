cdc2697c29fed76dec3d994960478249628d9ed3
Amin Foroutan <aminforoutan7@gmail.com>
2020-10-23 19:27:42 +0330

    first react setup

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Test from './test'
+const App = () => {
+  return (
+    <Test />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
