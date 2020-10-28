aef43fef6fd6df005aaa9281854f1cb7a578a434
Amin Foroutan <aminforoutan7@gmail.com>
2020-10-28 13:56:32 +0330

    added the profile route and bundle name for index page

--- Routes/index.js
+++ Routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import auth from './api/auth'
+import profile from './profile'
 const router = express.Router()
 
 export default () => {
   router.get('/', (req, res) => {
-    res.render('index', { title: 'homepage' })
+    res.render('index', { title: 'homepage', bundle: 'home' })
   })
 
+  router.use('/profile', profile())
   router.use('/login', auth())
 
   return router
