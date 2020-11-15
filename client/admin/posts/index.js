75a2902ece0d60b851d43ed06b8a69b6ce2c4984
amin foroutan <aminforoutan7@gmail.com>
2020-11-15 19:03:05 +0330

    added the fetch for all posts in pos lists

--- client/admin/posts/index.js
+++ client/admin/posts/index.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import PostItem from './PostItem'
+import axios from 'axios'
 class AllPosts extends Component {
+  constructor () {
+    super()
+    this.state = {
+      loaded: false,
+      posts: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('/posts').then(res => {
+      this.setState({
+        ...this.state,
+        loaded: true,
+        posts: res.data.posts
+      })
+      console.log(res.data.posts)
+    })
+  }
+
   render () {
-    return (
-      <div>
-        <div>posts</div>
-        <div>posts</div>
-        <div>posts</div>
-        <div>posts</div>
-      </div>
-    )
+    if (this.state.loaded) {
+      return (
+        <div className="post-list">
+          {this.state.posts.map((post, i) => {
+            return (<div key={i}><PostItem title={post.title} id={post.id} /></div>)
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          ...loading
+        </div>
+      )
+    }
   }
 }
 
