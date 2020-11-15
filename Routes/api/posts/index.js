aeb0204a86d696521d0328503b60e6371aaea423
amin foroutan <aminforoutan7@gmail.com>
2020-11-15 19:00:27 +0330

    added the posts api [check desc] -the post and put and get

--- Routes/api/posts/index.js
+++ Routes/api/posts/index.js
@@ -6,14 +6,35 @@ import Post from '../../../Models/Post'
 const router = express.Router()
 
 export default () => {
-  router.get('/', (req, res) => {
-    res.render('works/archive', { title: 'works' })
+  router.get('/', async (req, res) => {
+    try {
+      const result = await Post.find()
+      const posts = result.map(item => {
+        return {
+          title: item.title,
+          createdAt: item.createdAt,
+          id: item.id,
+          published: item.published,
+          tags: item.tags,
+          author: item.author
+        }
+      })
+      res.json({ posts: posts })
+    } catch (e) {
+      console.log(e)
+    }
   })
 
-  router.get('/:post', (req, res) => {
+  router.get('/:post', async (req, res) => {
     const { post } = req.params
-    console.log(post)
-    res.json({ test: post })
+    try {
+      const result = await Post.findOne({
+        id: post
+      })
+      res.json({ post: result })
+    } catch (e) {
+      console.log(e)
+    }
   })
 
   router.get('/exists/:id', async (req, res) => {
@@ -26,6 +47,57 @@ export default () => {
     } catch (e) {
       console.log(e)
       res.sendStatus(500)
+    }
+  })
+
+  router.put('/', async (req, res) => {
+    const dataExpected = {
+      title: true,
+      body: true,
+      tags: true,
+      author: true,
+      published: true,
+      id: true
+    }
+    const dataRequired = {
+      title: true,
+      body: true,
+      author: true,
+      published: true,
+      id: true
+    }
+
+    for (const key in dataRequired) {
+      if (!(key in req.body)) {
+        res.status(422).json({ message: `${key} parameter is required` })
+      }
+    }
+    for (const key in req.body) {
+      if (!dataExpected[key]) {
+        res.status(422).json({ message: `Wrong body parameters: ${key}` })
+      }
+    }
+
+    const { title, body, tags, author, published, id } = req.body
+
+    try {
+      const result = await Post.findOne({ id: id })
+      result.title = title
+      result.id = title
+      result.body = body
+      result.tags = tags
+      result.author = author
+      result.published = published
+      const updated = await result.save()
+      res.json({
+        success: true,
+        id: updated.id
+      })
+    } catch (e) {
+      res.json({
+        error: e.message,
+        success: false
+      })
     }
   })
 
@@ -69,14 +141,13 @@ export default () => {
       const result = await newPost.save()
       console.log(result)
       res.json({
-        title,
-        body,
-        tags,
-        author,
-        published
+        success: true
       })
     } catch (e) {
-      res.json(e)
+      res.json({
+        error: e.message,
+        success: false
+      })
     }
   })
 
